baead09edbc43d6ee85051480bda82e51d6953f2
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-02-15 14:37:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import { store } from './store'
+
+export default {
+  data() {
+    return {
+      store
+
+    }
+  }
+}
+</script>
+
+<template>
+  <span>dsgs</span>
+</template>
+
+<style lang="scss">
+@use './assets/style/genral.scss'
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+
+export const store = reactive ({
+    api: {
+        uri: '',
+        key: '',
+    }
+})
